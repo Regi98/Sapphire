4a6671b7a7aedc70d78d433ce8960eab49d77eba
Mikhaela Maun <gncrnmn@gmail.com>
2018-07-26 08:33:14 +0800

    fixed wishlist

--- shop/my-wishlist.php
+++ shop/my-wishlist.php
@@ -39,8 +39,9 @@ $id= $_SESSION['id'];
       $num=mysqli_fetch_assoc($results);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+	<!DOCTYPE html>
+	<html lang="en">
+
 	<head>
 		<!-- Meta -->
 		<meta charset="utf-8">
@@ -48,16 +49,16 @@ $id= $_SESSION['id'];
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<meta name="description" content="">
 		<meta name="author" content="">
-	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
-	    <meta name="robots" content="all">
+		<meta name="keywords" content="MediaCenter, Template, eCommerce">
+		<meta name="robots" content="all">
 
-	    <title>My Wishlist</title>
-	    <!--<link rel="stylesheet" href="assets/css/bootstrap.min.css">-->
-	    
-	    <!-- Customizable CSS -->
-	    <link rel="stylesheet" href="assets/css/main.css">
-	    <link rel="stylesheet" href="assets/css/blue.css">
-	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<title>My Wishlist</title>
+		<!--<link rel="stylesheet" href="assets/css/bootstrap.min.css">-->
+
+		<!-- Customizable CSS -->
+		<link rel="stylesheet" href="assets/css/main.css">
+		<link rel="stylesheet" href="assets/css/blue.css">
+		<link rel="stylesheet" href="assets/css/owl.carousel.css">
 		<link rel="stylesheet" href="assets/css/owl.transitions.css">
 		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
 		<link href="assets/css/lightbox.css" rel="stylesheet">
@@ -75,11 +76,11 @@ $id= $_SESSION['id'];
 		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
 		<!-- Demo Purpose Only. Should be removed in production : END -->
 
-		
+
 		<!-- Icons/Glyphs -->
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
-        <!-- Fonts --> 
+		<!-- Fonts -->
 		<link rel="stylesheet" href="assets/css/myfont.css">
 
 		<!-- NAVBAR -->
@@ -97,147 +98,167 @@ $id= $_SESSION['id'];
 
 		<link rel="shortcut icon" href="assets/images/favicon.ico">
 	</head>
-    <body class="cnt-home">
 
-    	<?php include('includes/header.php');?>
+	<body class="cnt-home">
 
-<header class="header-style-1">
+		<?php include('includes/header.php');?>
 
-	<!-- ============================================== TOP MENU ============================================== -->
-<?php include('includes/top-header.php');?>
-<!-- ============================================== TOP MENU : END ============================================== -->
-<?php include('includes/main-header.php');?>
-	<!-- ============================================== NAVBAR ============================================== -->
-<?php include('includes/menu-bar.php');?>
-<!-- ============================================== NAVBAR : END ============================================== -->
+		<header class="header-style-1">
 
-</header>
+			<!-- ============================================== TOP MENU ============================================== -->
+			<?php include('includes/top-header.php');?>
+			<!-- ============================================== TOP MENU : END ============================================== -->
+			<?php include('includes/main-header.php');?>
+			<!-- ============================================== NAVBAR ============================================== -->
+			<?php include('includes/menu-bar.php');?>
+			<!-- ============================================== NAVBAR : END ============================================== -->
 
-<!-- ============================================== HEADER : END ============================================== -->
-<div class="breadcrumb">
-	<div class="container">
-		<div class="breadcrumb-inner">
-			<ul class="list-inline list-unstyled">
-				<li><a href="home.html">Home</a></li>
-				<li class='active'>Wishlish</li>
-			</ul>
-		</div><!-- /.breadcrumb-inner -->
-	</div><!-- /.container -->
-</div><!-- /.breadcrumb -->
+		</header>
 
-<div class="body-content outer-top-bd">
-	<div class="container">
-		<div class="my-wishlist-page inner-bottom-sm">
-			<div class="row">
-				<div class="col-md-12 my-wishlist">
-	<div class="table-responsive">
-		<table class="table">
-			<thead>
-				<tr>
-					<th colspan="4">my wishlist</th>
-				</tr>
-			</thead>
-			<tbody>
-<?php
-$ret=mysqli_query($con,"select products.productName as pname,products.productName as proid,products.productImage1 as pimage,products.productPrice as pprice,wishlist.productId as pid,wishlist.id as wid from wishlist join products on products.id=wishlist.productId where wishlist.userId='".$_SESSION['id']."'");
+		<!-- ============================================== HEADER : END ============================================== -->
+		<div class="breadcrumb">
+			<div class="container">
+				<nav aria-label="breadcrumb pull-right">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="index.php">Shop</a></li>
+						<li class="breadcrumb-item active" aria-current="page">My Wishlist</li>
+					</ol>
+				</nav>
+				<!-- /.breadcrumb-inner -->
+			</div>
+			<!-- /.container -->
+		</div>
+		<!-- /.breadcrumb -->
+
+		<div class="body-content outer-top-bd">
+			<div class="container">
+				<div class="my-wishlist-page inner-bottom-sm">
+					<div class="row">
+						<div class="col-md-12 my-wishlist">
+							<div class="table-responsive">
+								<table class="table">
+									<thead>
+										<tr>
+											<th colspan="4">my wishlist</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php
+$ret=mysqli_query($con,"select products.product_name as pname,products.id as proid,products.product_image_1 as pimage,products.	product_price as pprice,wishlist.productId as pid,wishlist.id as wid from wishlist join products on products.id=wishlist.productId where wishlist.userId='".$_SESSION['id']."'");
 $num=mysqli_num_rows($ret);
 	if($num>0)
 	{
 while ($row=mysqli_fetch_array($ret)) {
 
 ?>
-
-				<tr>
-					<td class="col-md-2"><img src="../../inflightapp/storage/app/public/product_images/<?php echo htmlentities($row['proid']);?>/<?php echo htmlentities($row['pimage']);?>" alt="<?php echo htmlentities($row['pname']);?>" width="60" height="100"></td>
-					<td class="col-md-6">
-						<div class="product-name"><a href="product-details.php?pid=<?php echo htmlentities($pd=$row['pid']);?>"><?php echo htmlentities($row['pname']);?></a></div>
-<?php $rt=mysqli_query($con,"select * from productreviews where productId='$pd'");
+											<tr>
+												<td>
+													<img src="../../inflightapp/storage/app/public/product_images/<?php echo htmlentities($row['pimage']);?>" alt="<?php echo htmlentities($row['pname']);?>"
+													    width="100" height="100">
+												</td>
+												<td>
+													<div class="product-name">
+														<a href="product-details.php?pid=<?php echo htmlentities($pd=$row['pid']);?>">
+															<?php echo htmlentities($row['pname']);?>
+														</a>
+													</div>
+													<?php $rt=mysqli_query($con,"select * from productreviews where productId='$pd'");
 $num=mysqli_num_rows($rt);
 {
 ?>
 
-						<div class="rating">
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star non-rate"></i>
-							<span class="review">( <?php echo htmlentities($num);?> Reviews )</span>
+													<div class="rating">
+														<i class="fa fa-star rate"></i>
+														<i class="fa fa-star rate"></i>
+														<i class="fa fa-star rate"></i>
+														<i class="fa fa-star rate"></i>
+														<i class="fa fa-star non-rate"></i>
+														<span class="review">(
+															<?php echo htmlentities($num);?> Reviews )</span>
+													</div>
+													<?php } ?>
+													<div class="price">$
+														<?php echo htmlentities($row['pprice']);?>.00
+														<span>$900.00</span>
+													</div>
+												</td>
+												<td>
+													<a href="my-wishlist.php?page=product&action=add&id=<?php echo $row['pid']; ?>" class="btn-upper btn btn-primary">Add to cart</a>
+												</td>
+												<td class=" close-btn">
+													<a href="my-wishlist.php?del=<?php echo htmlentities($row['wid']);?>" onClick="return confirm('Are you sure you want to delete?')"
+													    class="">
+														<i class="fa fa-times"></i>
+													</a>
+												</td>
+											</tr>
+											<?php } } else{ ?>
+											<tr>
+												<td style="font-size: 18px; font-weight:bold ">Your Wishlist is Empty</td>
+
+											</tr>
+											<?php } ?>
+									</tbody>
+								</table>
+							</div>
 						</div>
-						<?php } ?>
-						<div class="price">$
-							<?php echo htmlentities($row['pprice']);?>.00
-							<span>$900.00</span>
-						</div>
-					</td>
-					<td class="col-md-2">
-						<a href="my-wishlist.php?page=product&action=add&id=<?php echo $row['pid']; ?>" class="btn-upper btn btn-primary">Add to cart</a>
-					</td>
-					<td class="col-md-2 close-btn">
-						<a href="my-wishlist.php?del=<?php echo htmlentities($row['wid']);?>" onClick="return confirm('Are you sure you want to delete?')" class=""><i class="fa fa-times"></i></a>
-					</td>
-				</tr>
-				<?php } } else{ ?>
-				<tr>
-					<td style="font-size: 18px; font-weight:bold ">Your Wishlist is Empty</td>
+					</div>
+					<!-- /.row -->
+				</div>
+				<!-- /.sigin-in-->
+			</div>
+		</div>
+		</section>
 
-				</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</div>
-</div>			</div><!-- /.row -->
-		</div><!-- /.sigin-in-->
-</div>
-</div>
-</section>
+		<?php include('includes/footer.php');?>
 
-<?php include('includes/footer.php');?>
+		<script src="assets/js/jquery-1.11.1.min.js"></script>
 
-	<script src="assets/js/jquery-1.11.1.min.js"></script>
-	
-	<script src="assets/js/bootstrap.min.js"></script>
-	
-	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
-	<script src="assets/js/owl.carousel.min.js"></script>
-	
-	<script src="assets/js/echo.min.js"></script>
-	<script src="assets/js/jquery.easing-1.3.min.js"></script>
-	<script src="assets/js/bootstrap-slider.min.js"></script>
-    <script src="assets/js/jquery.rateit.min.js"></script>
-    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
-    <script src="assets/js/bootstrap-select.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-	<script src="assets/js/scripts.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 
-	<!-- For demo purposes – can be removed on production -->
-	
-	<script src="switchstylesheet/switchstylesheet.js"></script>
-	
-	<script>
-		$(document).ready(function(){ 
-			$(".changecolor").switchstylesheet( { seperator:"color"} );
-			$('.show-theme-options').click(function(){
-				$(this).parent().toggleClass('open');
-				return false;
+		<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+		<script src="assets/js/owl.carousel.min.js"></script>
+
+		<script src="assets/js/echo.min.js"></script>
+		<script src="assets/js/jquery.easing-1.3.min.js"></script>
+		<script src="assets/js/bootstrap-slider.min.js"></script>
+		<script src="assets/js/jquery.rateit.min.js"></script>
+		<script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+		<script src="assets/js/bootstrap-select.min.js"></script>
+		<script src="assets/js/wow.min.js"></script>
+		<script src="assets/js/scripts.js"></script>
+
+		<!-- For demo purposes – can be removed on production -->
+
+		<script src="switchstylesheet/switchstylesheet.js"></script>
+
+		<script>
+			$(document).ready(function () {
+				$(".changecolor").switchstylesheet({
+					seperator: "color"
+				});
+				$('.show-theme-options').click(function () {
+					$(this).parent().toggleClass('open');
+					return false;
+				});
 			});
-		});
 
-		$(window).bind("load", function() {
-		   $('.show-theme-options').delay(2000).trigger('click');
-		});
-	</script>
+			$(window).bind("load", function () {
+				$('.show-theme-options').delay(2000).trigger('click');
+			});
+		</script>
 
- 	<!-- NAVBAR SCRIPTS -->
-	<script src="distribution/vendor/jquery/jquery.min.js"></script>
-	<script src="distribution/vendor/popper.js/umd/popper.min.js">
-	</script>
-	<script src="distribution/vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="distribution/vendor/jquery.cookie/jquery.cookie.js">
-	</script>
-	<script src="distribution/vendor/jquery-validation/jquery.validate.min.js"></script>
-	<script src="distribution/js/front.js"></script>
-	
-</body>
-</html>
-<?php } ?>
+		<!-- NAVBAR SCRIPTS -->
+		<script src="distribution/vendor/jquery/jquery.min.js"></script>
+		<script src="distribution/vendor/popper.js/umd/popper.min.js">
+		</script>
+		<script src="distribution/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="distribution/vendor/jquery.cookie/jquery.cookie.js">
+		</script>
+		<script src="distribution/vendor/jquery-validation/jquery.validate.min.js"></script>
+		<script src="distribution/js/front.js"></script>
+
+	</body>
+
+	</html>
+	<?php } ?>
