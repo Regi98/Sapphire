5d39e7ad26c449a13987684d007448e4c0e6bdad
Mikhaela Maun <gncrnmn@gmail.com>
2018-07-27 13:42:42 +0800

    fixed order track history count

--- shop/admin/include/sidebar.php
+++ shop/admin/include/sidebar.php
@@ -10,24 +10,29 @@
 			{
 			?>
 			<?php echo htmlentities($num_rows1); ?>
-		<?php } ?> New</span></h5></p></a>
+		<?php } ?> Orders</span></h5></p></a>
+
       <a href="pending-orders.php" class="w3-bar-item w3-button"><i class="fas fa-tasks"></i><p  style="font-size: 0.6rem;">Pending Orders<h5><span style="font-size: 0.8rem;" class="badge badge-warning"><?php	
 			// $status='Delivered';									 
 		$ret = mysqli_query($con,"SELECT * FROM Orders where orderStatus is null ");
 		$num = mysqli_num_rows($ret);
 		{?><?php echo htmlentities($num); ?>
-		<?php } ?> New</span></h5></p></a>
+		<?php } ?> Orders</span></h5></p></a>
+
       <a href="delivered-orders.php" class="w3-bar-item w3-button"><i class="fas fa-inbox"></i><p  style="font-size: 0.6rem;">Delivered Orders<h5><span style="font-size: 0.8rem;" class="badge badge-success"><?php	
 		$status='Delivered';									 
 		$rt = mysqli_query($con,"SELECT * FROM Orders where orderStatus='$status'");
 		$num1 = mysqli_num_rows($rt);
 		{?><?php echo htmlentities($num1); ?>
-		<?php } ?> New</span></h5></p></a>
+		<?php } ?> Orders</span></h5></p></a>
+
 		<a href="returned-cancelled-orders.php" class="w3-bar-item w3-button"><i class="fas fa-times-circle"></i><p  style="font-size: 0.6rem;">Returned and Cancelled Orders<h5><span style="font-size: 0.8rem;" class="badge badge-success"><?php	
-		$status='Delivered';									 
-		$rt = mysqli_query($con,"SELECT * FROM Orders where orderStatus='$status'");
-		$num1 = mysqli_num_rows($rt);
-		{?><?php echo htmlentities($num1); ?>
-		<?php } ?> New</span></h5></p></a>
+		$status1='Returned';	
+		$status2='Cancelled';									 
+		$rtt = mysqli_query($con,"SELECT * FROM ordertrackhistory");
+		$num2 = mysqli_num_rows($rtt);
+		{?><?php echo htmlentities($num2); ?>
+		<?php } ?> Orders</span></h5></p></a>
+
       <a href="products-in-stock.php" class="w3-bar-item w3-button"><i class="fas fa-boxes"></i><p  style="font-size: 0.6rem;">Products in Stock</p></a>
     </div>
